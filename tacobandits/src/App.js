298d6eff6f951ebd9a770bcb3d5b8e4185c88d75
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-24 17:55:00 -0400

    react-app basic set up

---
+++ tacobandits/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Taco Bandits game!</h1>
+    </div>
+  );
+}
+
+export default App;
