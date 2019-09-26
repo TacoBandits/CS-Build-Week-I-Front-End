4d1408fa7880e93c75a0857bdd671948073a260b
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-26 14:37:04 -0400

    added react router/routes/navlinks

--- tacobandits/src/App.js
+++ tacobandits/src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import World from './components/World';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the Taco Bandits game!</h1>
-      <p>. . . where you must escape from Tacocat, the Purrrrrrveyor of Palindromes!</p>
-      <Login />
-      <Register />
-    </div>
-  );
+class App extends React.Component {
+
+  logout = () => {
+    localStorage.removeItem('key');
+    this.props.history.push('/login');
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <NavLink to='/register'>Sign-Up</NavLink>
+            <NavLink to='/login'>Log-In</NavLink>
+            <NavLink to='/game'>Play!</NavLink>
+            <p onClick={this.logout}>Log-Out</p>
+          </nav>
+        </header>
+        
+        <h1>Welcome to the Taco Bandits game!</h1>
+        <p>. . . where you must escape from Tacocat, the Purrrrrrveyor of Palindromes!</p>
+        <div>
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='/game' component={World} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
