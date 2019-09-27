ffb51147ce77fd49cc1816123014a880f62e757c
meginks <meganjones.dev@gmail.com>
2019-09-26 23:24:40 -0500

    fixed some language and added some styles

--- tacobandits/src/components/Register.js
+++ tacobandits/src/components/Register.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import tacocat from '../images/TACOCAT.svg';
+import {Image, Form, Button} from 'semantic-ui-react';
 
 class Register extends React.Component {
 	state = {
@@ -29,35 +31,41 @@ class Register extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2>Welcome, Please Register!</h2>
-				<form onSubmit={this.handleSubmit}>
-					
-					<input 
+				<h2>I am Madam Tacocat, Purrrrrveyor of Palindromes! Please register to help me escape!</h2>
+				<Image size="mini" src={tacocat} style={{width: "250px", height: "250px"}} /> 
+				<Form onSubmit={this.handleSubmit}>
+					<Form.Field>
+					<Form.Input 
 						label='username'
-            name='username'
+            			name='username'
 						id='username'
 						type='text'
 						value={this.state.username}
 						onChange={this.handleChange}
 					/>
-					<input 
+					</Form.Field>
+					<Form.Field>
+					<Form.Input 
 						label='password1' 
-            name='password1'
+            			name='password1'
 						id='password1'
 						type='password'
 						value={this.state.password1}
 						onChange={this.handleChange}
 					/>
-          <input 
+					</Form.Field>
+					<Form.Field>
+          			<Form.Input 
 						label='password2'
-            name='password2' 
+            			name='password2' 
 						id='password2'
 						type='password'
 						value={this.state.password2}
 						onChange={this.handleChange}
 					/>
-					<button type='submit'>submit</button>
-				</form>
+					</Form.Field>
+					<Button type='submit'>Help tacocat escape!</Button>
+				</Form>
 			</div>
 		)
 	}
